d04f0f79d54da9efd7f38573596db4c76d42662c
Thomas Couto (LAPTOP) <thomascouto@gmail.com>
2023-11-02 20:08:04 -0300

    chore: docker config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: 'standalone', // docker
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
